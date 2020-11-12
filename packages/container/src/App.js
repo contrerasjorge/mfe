4eab6e270145308b15642beee862ced36ce30d6a
Jorge Contreras <jorge.contreras789@gmail.com>
2020-11-12 11:09:20 -0500

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>hey yo how ya doin'?</h1>
+      <h1>hey yo how ya doin'!?</h1>
       <hr />
       <MarketingApp />
     </div>
